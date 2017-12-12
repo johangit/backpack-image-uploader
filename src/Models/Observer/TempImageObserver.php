08e2568bf811d7db7680ffa03ad16808c34b46d9
johan <mihailbulin@gmail.com>
2017-12-13 04:09:09 +0700

    remove old records

--- src/Models/Observer/TempImageObserver.php
+++ src/Models/Observer/TempImageObserver.php
@@ -13,6 +13,11 @@ class TempImageObserver
 
     public function saved(TempImage $image)
     {
+        $date = new \Carbon\Carbon('10 day ago');
+
+        TempImage::where("created_at", "<", $date->toDateString())
+            ->take(100)
+            ->delete();
     }
 
     public function deleting(TempImage $image)
